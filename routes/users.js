b2086e19603fbbc244d175f2c9f7ddbafa2984d5
Elena Kocheriagina <lunaticluntic@yandex.ru>
2022-08-04 19:53:12 +0300

    first reviw

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,16 @@
-const usersRouter = require("express").Router();
+const usersRouter = require('express').Router();
 const {
   createUser,
   getUsers,
   getUserId,
   updateUserInfo,
   updateUserAvatar,
-} = require("../controllers/users");
+} = require('../controllers/users');
 
-usersRouter.get("/users", getUsers);
-usersRouter.post("/users", createUser);
-usersRouter.get("/users/:id", getUserId);
-usersRouter.patch("/users/me/avatar", updateUserAvatar);
-usersRouter.patch("/users/me", updateUserInfo);
+usersRouter.get('/users', getUsers);
+usersRouter.post('/users', createUser);
+usersRouter.get('/users/:id', getUserId);
+usersRouter.patch('/users/me/avatar', updateUserAvatar);
+usersRouter.patch('/users/me', updateUserInfo);
 
 module.exports = usersRouter;
