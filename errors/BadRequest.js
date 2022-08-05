9d713aa5ab4429ae127b33d42be40dcf7390a4b9
Elena Kocheriagina <lunaticluntic@yandex.ru>
2022-08-05 10:51:43 +0300

    правки после 1 ревью

--- errors/BadRequest.js
+++ errors/BadRequest.js
@@ -1,7 +1,7 @@
-class BadReques extends Error {
+class BadRequest extends Error {
   constructor(message) {
     super(message);
     this.statusCode = 400;
   }
 }
-module.exports = BadReques;
+module.exports = BadRequest;
